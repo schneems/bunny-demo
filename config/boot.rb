c7dd20f157c98d28992276546036e0ae49ee23f0
schneems <richard.schneeman@gmail.com>
2015-12-18 12:01:55 -0600

    sync-stdout

--- config/boot.rb
+++ config/boot.rb
@@ -1,8 +1,12 @@
 require "bunny"
 
+STDOUT.sync = true
+
 $bunny_connection = Bunny.new(ENV['AMQP_URL'] || ENV['CLOUDAMQP_URL'] || 'amqp://127.0.0.1:5672', automatically_recover: false )
 $bunny_connection.start
 
 $bunny_channel   = $bunny_connection.create_channel
 $bunny_queue     = $bunny_channel.queue("bunny.examples.hello_world", :auto_delete => true)
 $bunny_exchange  = $bunny_channel.default_exchange
+
+
